82bc176a06195347ceb086a62f96f301148e2bfc
Sarath <sarath.hacks@gmail.com>
2017-04-13 15:11:33 +0530

    Added redirect button

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'
 
 export default class About extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { auth: true };
+  }
+
+  gobackToHome() {
+
+    this.setState({auth:false});
+
+  }
+
   render() {
+
+    if(!this.state.auth) {
+      return(
+        <Redirect to="/"></Redirect>
+      );
+    }
+
     return(
       <div className="page">
         <h1>About</h1>
+        <button onClick={this.gobackToHome.bind(this)}>Back to Home</button>
       </div>
     )
   }
